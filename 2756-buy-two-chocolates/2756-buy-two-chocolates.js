80afecbf990c6acbdb2fb01ab109b2899b16652c
Habeeba-Husna <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-09 16:52:29 +0530

    Time: 15 ms (6.08%), Space: 55.4 MB (14.7%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.js
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.js
@@ -4,11 +4,15 @@
  * @return {number}
  */
 var buyChoco = function(prices, money) {
-      prices.sort((a,b)=>a-b);
+    //   prices.sort((a,b)=>a-b);
+    // let sum=prices[0]+prices[1];
+    // if(sum<=money){
+    //     return money-sum;
+    // }else{
+    //     return money;
+    // }
+
+    let a=prices.sort((a,b)=>a-b);
     let sum=prices[0]+prices[1];
-    if(sum<=money){
-        return money-sum;
-    }else{
-        return money;
-    }
+    return (money-sum)>=0?(money-sum):money;
 };

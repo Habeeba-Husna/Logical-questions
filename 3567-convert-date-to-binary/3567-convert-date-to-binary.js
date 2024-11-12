56994a1372397c600db2413ffacd241ceb5c9cde
Habeeba-Husna <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-12 09:53:29 +0530

    Time: 2 ms (22.88%), Space: 50 MB (75.07%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.js
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.js
@@ -3,11 +3,14 @@
  * @return {string}
  */
 var convertDateToBinary = function(date) {
-    const [year,month,day]=date.split('-')
-    const y=parseInt(year,10).toString(2)
-    const m=parseInt(month,10).toString(2)
-    const d=parseInt(day,10).toString(2)
-    const fullDate=`${y}-${m}-${d}`
-    return fullDate;
+    // const [year,month,day]=date.split('-')
+    // const y=parseInt(year,10).toString(2)
+    // const m=parseInt(month,10).toString(2)
+    // const d=parseInt(day,10).toString(2)
+    // const fullDate=`${y}-${m}-${d}`
+    // return fullDate;
+
+    return date.split('-').map(num=>Number(num,10).toString(2)).join('-')
+
 
 };

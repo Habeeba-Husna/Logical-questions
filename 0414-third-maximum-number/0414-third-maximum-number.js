f098ee27625b602567593789242d7ae9686c88f7
Habeeba-Husna <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-16 10:00:07 +0530

    Time: 1 ms (89.82%), Space: 50.8 MB (74.64%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+   let sorted = nums.sort((a,b)=> b-a)
+//    console.log(sorted)
+    let unique = [...new Set(sorted)];
+    // console.log(unique)
+    // if(unique.length >=3) return unique[2]
+    // return unique[0]
+    return unique.length>=3?unique[2]:unique[0]
+};

9bcbfaeeaeb37e7afd52679230594254b95b9f7d
Habeeba-Husna <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-20 09:53:45 +0530

    Time: 13 ms (9.39%), Space: 50.2 MB (28.72%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -4,15 +4,27 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
-       let count = 0;
-    for(let i=1,j=0; i<=arr.length+k;i++){
-        if(arr[j] && arr[j]===i){
-            j++;
-        }else{
-            count++;
-            if(count===k){
-                return i
-            }
-        }
-    }
+    //    let count = 0;
+    // for(let i=1,j=0; i<=arr.length+k;i++){
+    //     if(arr[j] && arr[j]===i){
+    //         j++;
+    //     }else{
+    //         count++;
+    //         if(count===k){
+    //             return i
+    //         }
+    //     }
+    // }
+
+
+
+    let count=0;
+    let newArr=[];
+    while(count<=arr.length+k){
+        count++;
+    
+    if(!arr.includes(count)){
+        newArr.push(count)
+    }}
+    return newArr[k-1];
 };

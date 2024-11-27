2a7eeaf82602f31cad8c050900e6f01a8437a699
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-27 10:26:22 +0530

    Time: 66 ms (5.4%), Space: 58.3 MB (5.07%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,14 +3,14 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    //   let a = s.split(" ");
-    //   console.log(a)
-    // for(let i = 0; i < a.length; i++){
-    //     a[i]=a[i].split("").reverse().join("");
-    //     console.log(a[i])
-    // }
-    // return a.join(" ");
+      let a = s.split(" ");
+      console.log(a)
+    for(let i = 0; i < a.length; i++){
+        a[i]=a[i].split("").reverse().join("");
+        console.log(a[i])
+    }
+    return a.join(" ");
     
-     return s.split(' ').map((a)=>a.split('').reverse().join('')).join(' ')
+    //  return s.split(' ').map((a)=>a.split('').reverse().join('')).join(' ')
     
 };

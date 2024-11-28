c41077145b362fa6cd6043d9f5202d519e5d3064
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-28 10:00:56 +0530

    Time: 0 ms (100%), Space: 48.9 MB (27.44%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    let a;
+     if (s.trim() === "") {
+        return 0;
+    }
+   return a =s.trim().split(/\s+/).length
+  console.log(a)
+   
+   
+    
+};

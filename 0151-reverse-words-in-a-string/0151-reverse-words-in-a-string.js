1a4cbc601bba2d9a01e7e5ed1304054d20c6ee58
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-11-28 09:37:19 +0530

    Time: 1 ms (88.35%), Space: 50.8 MB (67.79%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,7 +3,8 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let a=s.trim().split(' ').filter(word => word !== '').reverse().join(' ')
-    console.log(a);
-    return a
+    let a;
+    // return a=s.trim().split(' ').filter(word => word !== '').reverse().join(' ')
+    return a=s.trim().split(/\s+/).reverse().join(' ')
+    // console.log(a);
 };

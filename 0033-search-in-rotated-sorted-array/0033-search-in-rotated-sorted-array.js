1e34152e3059bc58d87b4c3970f9c1339c57b5ee
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-03 09:41:51 +0530

    Time: 0 ms (100%), Space: 48.7 MB (83.6%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let a=nums.includes(target)
+    return nums.indexOf(target)
+    
+};

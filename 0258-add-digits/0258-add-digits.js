d930084648d81fb8a1fe51870bc5a1deb9269f8b
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-04 10:59:58 +0530

    Time: 0 ms (100%), Space: 52 MB (98.77%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    let a=num.toString().split(' ')
+    // let a=num.toString().split(' ')
     if(num === 0) return 0
 
     return (num-1)%9 +1;
-    console.log(a)
+    // console.log(a)
 };

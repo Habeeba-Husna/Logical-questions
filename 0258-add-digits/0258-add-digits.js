7e869c06b12dfdaddf1e911306b39e199aa2782b
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-04 11:08:17 +0530

    Time: 0 ms (100%), Space: 53.7 MB (28.07%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -2,10 +2,23 @@
  * @param {number} num
  * @return {number}
  */
-var addDigits = function(num) {
-    // let a=num.toString().split(' ')
-    if(num === 0) return 0
+// var addDigits = function(num) {
+//     if(num === 0) return 0
+//     return (num-1)%9 +1;
+// };
 
-    return (num-1)%9 +1;
-    // console.log(a)
+
+// Digital Root(n)=1+((n−1)mod9)  // formula
+
+
+var addDigits = function(num) {
+    while (num >= 10) { 
+        let sum = 0;
+        while (num > 0) { 
+            sum += num % 10;
+            num = Math.floor(num / 10); 
+        }
+        num = sum;
+    }
+    return num;
 };

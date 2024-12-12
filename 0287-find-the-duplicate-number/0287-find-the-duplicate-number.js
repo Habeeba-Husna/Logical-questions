14684ed437c7d32e45fd4b91bb65672b25ef9370
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-12 09:57:51 +0530

    Time: 24 ms (35.1%), Space: 72.7 MB (11.15%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+//     for(let i=0;i<nums.length;i++){
+//    if(nums.indexOf(nums[i])!==nums.lastIndexOf(nums[i])) 
+//     return nums[i]
+   
+//     }
+
+    let a= new Set();
+    console.log(a)
+    for(let num of nums){
+        if(a.has(num)){
+            return num
+        }
+        a.add(num)
+    }
+    
+};

106b12bbb94865cab9ec0a3613c3d0a57c4e879a
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-13 10:30:40 +0530

    Time: 95 ms (5.2%), Space: 54.3 MB (5.23%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var reverse = function(x) {
-    // if(x<0){
-    //    (x.toString().split('').slice(1).reverse().join(''))*-1
-    
-    // }else if{
-    //    Number(x.toString().split('').reverse().join())
-    // }
+    //  const a =  x>0? parseInt(x.toString().split('').reverse().join('')): -1*parseInt((-1*x).toString().split('').reverse().join(''))
+    // if(a> Math.pow(2, 31)-1 || a< Math.pow(-2, 31)) {
+    //     return 0;
+    // }else{
+    //     return a;
     // }
     let reverse = Math.abs(x).toString().split('').reverse().join('');
     console.log(reverse)
     reversed = x < 0 ? -Number(reverse) : Number(reverse);
      console.log(reversed)
-     if (reversed < -(2 ** 31) || reversed > 2 ** 31 - 1) {
-    return 0;
+    //  if (reversed < -(2 ** 31) || reversed > 2 ** 31 - 1) {
+    // return 0;
+    if(reversed> Math.pow(2, 31)-1 || reversed< Math.pow(-2, 31)) {
+        return 0;
 }
 return reversed;
 };

3b8b47c0dfb0d54d958be551d7bc86aae53a81e2
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-26 23:02:49 +0530

    Time: 2 ms (83.66%), Space: 49.6 MB (31.68%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.js
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.js
@@ -3,19 +3,30 @@
  * @return {string[]}
  */
 var stringMatching = function(words) {
-     let a = []
+    //  let a = []
 
-    for (let i = 0; i < words.length; i++) {
-        for (let j = 0; j < words.length; j++) {
-            if (i !== j) {
-                if (words[i].includes(words[j])) {
-                    if (!a.includes(words[j])) {
-                        a.push(words[j])
-                        continue
-                    }
-                }
+    // for (let i = 0; i < words.length; i++) {
+    //     for (let j = 0; j < words.length; j++) {
+    //         if (i !== j) {
+    //             if (words[i].includes(words[j])) {
+    //                 if (!a.includes(words[j])) {
+    //                     a.push(words[j])
+    //                     continue
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // return a
+
+    let result=[];
+    for(let i=0;i<words.length;i++){
+        for(let j=0;j<words.length;j++){
+            if(i!==j && words[j].indexOf(words[i]) !== -1){
+                result.push(words[i])
+                break;
             }
         }
     }
-    return a
+    return result
 };

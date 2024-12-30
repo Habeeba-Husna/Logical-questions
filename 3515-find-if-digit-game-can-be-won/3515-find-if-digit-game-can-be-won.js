4573494236642da38ea3fd3ad9f786abcbe8ca61
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-30 10:36:39 +0530

    Time: 0 ms (100%), Space: 50.9 MB (91.94%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.js
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.js
@@ -3,31 +3,35 @@
  * @return {boolean}
  */
 var canAliceWin = function(nums) {
-    // let sum=0
-    // for(let i=0;i<nums.length-1;i++){
-    //     sum+=nums[i]
-    //     console.log(sum)
-    //     if(sum===nums[nums.length-1])return false
-        
-    //         // if(sum>nums[nums.length-1]) return false
- 
+    // let arr1=[]
+    // let arr2=[]
+    // for(let i of nums){
+    //     if(i>9){
+    //         arr1.push(i)     
+    //     }else{
+    //         arr2.push(i)
+    //     }
     // }
-    //     return true
-    
-    let arr1=[]
-    let arr2=[]
-    for(let i of nums){
-        if(i>9){
-            arr1.push(i)     
-        }else{
-            arr2.push(i)
+    // let sum1=arr1.reduce((acc,curr)=>acc+curr,0)
+    // let sum2=arr2.reduce((acc,curr)=>acc+curr,0)
+    // if(sum1===sum2){
+    //     return false
+    // }else
+    // return true
+
+let single = 0;
+    let double = 0;
+    for (let i = 0; i < nums.length; ++i) {
+        if (nums[i] < 10) {
+            single += nums[i]
+        } else {
+            double +=nums[i]
         }
     }
-    let sum1=arr1.reduce((acc,curr)=>acc+curr,0)
-    let sum2=arr2.reduce((acc,curr)=>acc+curr,0)
-    if(sum1===sum2){
-        return false
-    }else
-    return true
 
+    if (single === double) {
+        return false 
+    } else {
+        return true
+    }
 };

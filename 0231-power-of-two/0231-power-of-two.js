2ed1279ab3290156026f560689cf3059a47d4d18
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2024-12-30 10:42:50 +0530

    Time: 2 ms (28.91%), Space: 53.5 MB (37.75%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+//     if (n <= 0) return 0
+//   return ((n & (n - 1)) === 0)
+
+  for(let i=0;i<31;i++){
+        if(Math.pow(2,i)==n){
+            return(true);
+        }
+        }
+        return(false);
+};

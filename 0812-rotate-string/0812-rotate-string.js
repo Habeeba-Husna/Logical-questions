3254d7d708f33f85d1675d42df7fe09ddce86bc0
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-01-02 10:26:05 +0530

    Time: 2 ms (18.04%), Space: 49.1 MB (35.24%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+     if(s.length !== goal.length) return false;
+    let str = s.concat(s);
+    console.log(str)
+    return str.includes(goal);
+};

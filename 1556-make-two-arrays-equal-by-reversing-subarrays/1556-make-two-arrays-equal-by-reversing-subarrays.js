358c1315ce2013800770796c313b0d96d4b7b28b
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-01-07 09:41:52 +0530

    Time: 7 ms (55.23%), Space: 50.9 MB (81.23%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.js
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.js
@@ -6,7 +6,6 @@
 var canBeEqual = function(target, arr) {
     target=target.sort((a,b)=>a-b)
     arr=arr.sort((a,b)=>a-b)
-    console.log(arr)
     for (let i = 0; i < target.length; i++) {
         if (target[i] !== arr[i]) {
             return false; 

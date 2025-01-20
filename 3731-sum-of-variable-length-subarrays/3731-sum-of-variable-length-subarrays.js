1d3c269f6b51c7f1b349b21cfe67e67febb31c46
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-01-20 09:53:40 +0530

    Time: 2 ms (100%), Space: 51.5 MB (100%) - LeetHub

--- 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.js
+++ 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.js
@@ -3,12 +3,23 @@
  * @return {number}
  */
 var subarraySum = function(nums) {
-   let result = [];
-    for(let i=0;i<nums.length;i++){
-        let start = Math.max(0, i - nums[i]);
-        let subarray = nums.slice(start, i + 1);
-        let sum = subarray.reduce((acc, curr) => acc + curr, 0); 
-        result.push(sum);
+//    let result = [];
+//     for(let i=0;i<nums.length;i++){
+//         let start = Math.max(0, i - nums[i]);
+//         let subarray = nums.slice(start, i + 1);
+//         let sum = subarray.reduce((acc, curr) => acc + curr, 0); 
+//         result.push(sum);
+//     }
+//     return result.reduce((acc, curr) => acc + curr, 0);
+
+ let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const start = Math.max(0, i - nums[i]);
+    for (let j = start; j <= i; j++) {
+      result += nums[j];
     }
-    return result.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  return result;
 };

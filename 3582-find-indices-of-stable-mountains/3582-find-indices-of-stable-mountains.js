e295648cecc48def1bd2665c10d39e22a3f0e1b5
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-01-23 09:58:15 +0530

    Time: 1 ms (71.94%), Space: 52.3 MB (40.82%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} height
+ * @param {number} threshold
+ * @return {number[]}
+ */
+var stableMountains = function(height, threshold) {
+    let arr=[]
+    for(let i=0;i<height.length;i++){
+        if(height[i-1]>threshold)
+        arr.push(i)
+    }
+    return arr
+};

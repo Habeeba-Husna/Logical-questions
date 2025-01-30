6d828f16db07ca8be0e9747adfdbd3cb219a3761
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-01-30 09:43:48 +0530

    Time: 1 ms (98.68%), Space: 51.1 MB (75.53%) - LeetHub

--- 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.js
+++ 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.js
@@ -3,13 +3,23 @@
  * @return {number}
  */
 var findMaxK = function(nums) {
-    let numSet = new Set(nums);
-    let maxK = -1;
-    for (let num of nums) {
-        if (num > 0 && numSet.has(-num)) {
-            maxK = Math.max(maxK, num);
-        }
-    }
+    // let numSet = new Set(nums);
+    // let maxK = -1;
+    // for (let num of nums) {
+    //     if (num > 0 && numSet.has(-num)) {
+    //         maxK = Math.max(maxK, num);
+    //     }
+    // }
+    // return maxK;
 
-    return maxK;
+
+    let i = 0;
+    let value = -1;
+    while(i <nums.length){
+        if(nums[i] > value && nums.includes(-nums[i])){
+            value = nums[i];
+        }
+        i++;
+    }
+    return value;
 };

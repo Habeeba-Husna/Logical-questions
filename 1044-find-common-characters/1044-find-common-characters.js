e1bf164e2d0878001d728322dbf7bf5b3bcc5e29
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-02-19 09:50:03 +0530

    Time: 5 ms (83.38%), Space: 58.2 MB (12.92%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.js
+++ 1044-find-common-characters/1044-find-common-characters.js
@@ -3,22 +3,34 @@
  * @return {string[]}
  */
 var commonChars = function(words) {
-   let common = [...words[0]]; 
-   console.log(common)
-    
-    for (let i = 1; i < words.length; i++) {
-        let currentWord = words[i].split('');
-        console.log(currentWord)
-        common = common.filter(char => {
-            let index = currentWord.indexOf(char);
-            if (index !== -1) {
-                currentWord.splice(index, 1);
-                return true;
-            }
-            return false;
-        });
-        console.log(common)
+     const split = words[0].split('');
+    const arr = [];
+    for (let i = 0; i < split.length; i += 1) {
+        const current = split[i];
+        const condition = words.every((item) => item.includes(current))
+        if (condition) {
+            arr.push(current)
+            words = words.map((word) => word.replace(current, ""))
+        }
     }
     
-    return common;
+    return arr
+//    let common = [...words[0]]; 
+//    console.log(common)
+    
+//     for (let i = 1; i < words.length; i++) {
+//         let currentWord = words[i].split('');
+//         console.log(currentWord)
+//         common = common.filter(char => {
+//             let index = currentWord.indexOf(char);
+//             if (index !== -1) {
+//                 currentWord.splice(index, 1);
+//                 return true;
+//             }
+//             return false;
+//         });
+//         console.log(common)
+//     }
+    
+//     return common;
 };

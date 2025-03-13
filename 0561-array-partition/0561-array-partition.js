ee6e3451c8e493bf66a4bdb21e72c6eda7ea79e5
Habeeba Husna E P <152970172+Habeeba-Husna@users.noreply.github.com>
2025-03-13 10:27:15 +0530

    Time: 41 ms (62.45%), Space: 59.7 MB (28.99%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -5,15 +5,15 @@
 var arrayPairSum = function(nums) {
     nums.sort((a,b)=>a-b);
     let sum=0
-    for(let i=0;i<nums.length;i++){
-        if(i%2===0){
-            sum+=nums[i]
-        }
-    }
-    //  for (let i = 0; i < nums.length; i += 2) {
-    //     sum += nums[i]
+    // for(let i=0;i<nums.length;i++){
+    //     if(i%2===0){
+    //         sum+=nums[i]
+    //     }
+    // }
+     for (let i = 0; i < nums.length; i += 2) {
+        sum += nums[i]
         // let n = Math.min(nums[i],nums[i+1]);
         //     sum = sum+n
-    // }
+    }
     return sum
 };
